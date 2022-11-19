72684cab3ddaecf9341069d4e839b0a0752402c8
Devdutt M R <devdinstructor@gmail.com>
2022-11-19 11:49:51 +0530

    Implements the FirstIndexOf(inv) on an array.

---
+++ FirstIndexOf.cs
@@ -0,0 +1,28 @@
+namespace devops
+{
+    public partial class Array
+    {
+        private int[] array;
+        public Array(int[] array)
+        {
+            this.array = array;
+        }
+
+        public int FirstIndexOf(int number)
+        {
+            var indexOfNumber = -1;
+            for (var index = 0; index < this.array.Length; index++)
+            {
+                if (this.array[index] != num)
+                {
+                    continue;
+                }
+
+                indexOfNumber = index;
+                break;
+            }
+
+            return indexOfNumber;
+        }
+    }
+}
